39ef03dc4349ef33b4a3b7d0f101a07cdcede5b1
zhihua0322 <sunzhihua980322@gmail.com>
2021-10-02 10:44:24 +0800

    add MP2

---
+++ MP2/cmake-build-debug/CMakeFiles/mp2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mp2.dir/src/receiver_main.c.o"
+  "mp2"
+  "mp2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mp2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
